0725071d5e6a65b5b3d8191885ce34f0dffe3c87
Aldo Zang <aldo.zang@gmail.com>
2011-02-08 20:02:34 -0200

    ARLuxblend25 branch created

---
+++ src/luxrender/operators/__init__.py
@@ -0,0 +1,356 @@
+# -*- coding: utf8 -*-
+#
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+# --------------------------------------------------------------------------
+# Blender 2.5 LuxRender Add-On
+# --------------------------------------------------------------------------
+#
+# Authors:
+# Doug Hammond
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# ***** END GPL LICENCE BLOCK *****
+#
+# System Libs
+from math import sqrt, atan2
+
+# Blender Libs
+import bpy
+from presets import AddPresetBase
+
+# LuxRender Libs
+from luxrender.export.scene import SceneExporter
+
+# Per-IDPropertyGroup preset handling
+
+class LUXRENDER_MT_base(object):
+	preset_operator = "script.execute_preset"
+	def draw(self, context):
+		return bpy.types.Menu.draw_preset(self, context)
+
+class LUXRENDER_OT_preset_base(AddPresetBase):
+	pass
+
+class LUXRENDER_MT_presets_engine(LUXRENDER_MT_base, bpy.types.Menu):
+	bl_label = "LuxRender Engine Presets"
+	preset_subdir = "luxrender/engine"
+
+class LUXRENDER_OT_preset_engine_add(LUXRENDER_OT_preset_base, bpy.types.Operator):
+	'''Save the current settings as a preset'''
+	bl_idname = 'luxrender.preset_engine_add'
+	bl_label = 'Add LuxRender Engine settings preset'
+	preset_menu = 'LUXRENDER_MT_presets_engine'
+	preset_values = [
+		'bpy.context.scene.luxrender_engine.%s'%v['attr'] for v in bpy.types.luxrender_engine.get_exportable_properties()
+	] + [
+		'bpy.context.scene.luxrender_sampler.%s'%v['attr'] for v in bpy.types.luxrender_sampler.get_exportable_properties()
+	] + [
+		'bpy.context.scene.luxrender_integrator.%s'%v['attr'] for v in bpy.types.luxrender_integrator.get_exportable_properties()
+	] + [
+		'bpy.context.scene.luxrender_volumeintegrator.%s'%v['attr'] for v in bpy.types.luxrender_volumeintegrator.get_exportable_properties()
+	] + [
+		'bpy.context.scene.luxrender_filter.%s'%v['attr'] for v in bpy.types.luxrender_filter.get_exportable_properties()
+	] + [
+		'bpy.context.scene.luxrender_accelerator.%s'%v['attr'] for v in bpy.types.luxrender_accelerator.get_exportable_properties()
+	]
+	preset_subdir = 'luxrender/engine'
+
+class LUXRENDER_MT_presets_networking(LUXRENDER_MT_base, bpy.types.Menu):
+	bl_label = "LuxRender Networking Presets"
+	preset_subdir = "luxrender/networking"
+
+class LUXRENDER_OT_preset_networking_add(LUXRENDER_OT_preset_base, bpy.types.Operator):
+	'''Save the current settings as a preset'''
+	bl_idname = 'luxrender.preset_networking_add'
+	bl_label = 'Add LuxRender Networking settings preset'
+	preset_menu = 'LUXRENDER_MT_presets_networking'
+	preset_values = [
+		'bpy.context.scene.luxrender_networking.%s'%v['attr'] for v in bpy.types.luxrender_networking.get_exportable_properties()
+	]
+	preset_subdir = 'luxrender/networking'
+
+class LUXRENDER_MT_presets_material(LUXRENDER_MT_base, bpy.types.Menu):
+	bl_label = "LuxRender Material Presets"
+	preset_subdir = "luxrender/material"
+
+class LUXRENDER_OT_preset_material_add(LUXRENDER_OT_preset_base, bpy.types.Operator):
+	'''Save the current settings as a preset'''
+	bl_idname = 'luxrender.preset_material_add'
+	bl_label = 'Add LuxRender Material settings preset'
+	preset_menu = 'LUXRENDER_MT_presets_material'
+	preset_values = []
+	preset_subdir = 'luxrender/material'
+	
+	def execute(self, context):
+		pv = [
+			'bpy.context.material.luxrender_material.%s'%v['attr'] for v in bpy.types.luxrender_material.get_exportable_properties()
+		] + [
+			'bpy.context.material.luxrender_emission.%s'%v['attr'] for v in bpy.types.luxrender_emission.get_exportable_properties()
+		] + [
+			'bpy.context.material.luxrender_transparency.%s'%v['attr'] for v in bpy.types.luxrender_transparency.get_exportable_properties()
+		]
+		
+		# store only the sub-properties of the selected lux material type
+		lux_type = context.material.luxrender_material.type
+		sub_type = getattr(bpy.types, 'luxrender_mat_%s' % lux_type)
+		
+		pv.extend([
+			'bpy.context.material.luxrender_material.luxrender_mat_%s.%s'%(lux_type, v['attr']) for v in sub_type.get_exportable_properties()
+		])
+		
+		self.preset_values = pv
+		return super().execute(context)
+
+class LUXRENDER_MT_presets_texture(LUXRENDER_MT_base, bpy.types.Menu):
+	bl_label = "LuxRender Texture Presets"
+	preset_subdir = "luxrender/texture"
+
+class LUXRENDER_OT_preset_texture_add(LUXRENDER_OT_preset_base, bpy.types.Operator):
+	'''Save the current settings as a preset'''
+	bl_idname = 'luxrender.preset_texture_add'
+	bl_label = 'Add LuxRender Texture settings preset'
+	preset_menu = 'LUXRENDER_MT_presets_texture'
+	preset_values = []
+	preset_subdir = 'luxrender/texture'
+	
+	def execute(self, context):
+		pv = [
+			'bpy.context.texture.luxrender_texture.%s'%v['attr'] for v in bpy.types.luxrender_texture.get_exportable_properties()
+		]
+		
+		# store only the sub-properties of the selected lux texture type
+		lux_type = context.texture.luxrender_texture.type
+		sub_type = getattr(bpy.types, 'luxrender_tex_%s' % lux_type)
+		
+		features, junk = getattr(context.texture.luxrender_texture, 'luxrender_tex_%s' % lux_type).get_paramset(context.scene)
+		if '2DMAPPING' in features:
+			pv.extend([
+				'bpy.context.texture.luxrender_texture.luxrender_tex_mapping.%s'%v['attr'] for v in bpy.types.luxrender_tex_mapping.get_exportable_properties()
+			])
+		if '3DMAPPING' in features:
+			pv.extend([
+				'bpy.context.texture.luxrender_texture.luxrender_tex_transform.%s'%v['attr'] for v in bpy.types.luxrender_tex_transform.get_exportable_properties()
+			])
+		
+		pv.extend([
+			'bpy.context.texture.luxrender_texture.luxrender_tex_%s.%s'%(lux_type, v['attr']) for v in sub_type.get_exportable_properties()
+		])
+		
+		self.preset_values = pv
+		return super().execute(context)
+
+# Volume data handling
+class LUXRENDER_MT_presets_volume(LUXRENDER_MT_base, bpy.types.Menu):
+	bl_label = "LuxRender Volume Presets"
+	preset_subdir = "luxrender/volume"
+
+class LUXRENDER_OT_preset_volume_add(LUXRENDER_OT_preset_base, bpy.types.Operator):
+	'''Save the current settings as a preset'''
+	bl_idname = 'luxrender.preset_volume_add'
+	bl_label = 'Add LuxRender Volume settings preset'
+	preset_menu = 'LUXRENDER_MT_presets_volume'
+	preset_values = []
+	preset_subdir = 'luxrender/volume'
+	
+	def execute(self, context):
+		ks = 'bpy.context.scene.luxrender_volumes.volumes[bpy.context.scene.luxrender_volumes.volumes_index].%s'
+		pv = [
+			ks%v['attr'] for v in bpy.types.luxrender_volume_data.get_exportable_properties()
+		]
+		
+		self.preset_values = pv
+		return super().execute(context)
+
+class LUXRENDER_OT_volume_add(bpy.types.Operator):
+	'''Add a new material volume definition to the scene'''
+	
+	bl_idname = "luxrender.volume_add"
+	bl_label = "Add LuxRender Volume"
+	
+	new_volume_name = bpy.props.StringProperty(default='New Volume')
+	
+	def invoke(self, context, event):
+		v = context.scene.luxrender_volumes.volumes
+		v.add()
+		new_vol = v[len(v)-1]
+		new_vol.name = self.properties.new_volume_name
+		return {'FINISHED'}
+	
+class LUXRENDER_OT_volume_remove(bpy.types.Operator):
+	'''Remove the selected material volume definition'''
+	
+	bl_idname = "luxrender.volume_remove"
+	bl_label = "Remove LuxRender Volume"
+	
+	def invoke(self, context, event):
+		w = context.scene.luxrender_volumes
+		w.volumes.remove( w.volumes_index )
+		w.volumes_index = len(w.volumes)-1
+		return {'FINISHED'}
+
+class LUXRENDER_OT_lookat_file(bpy.types.Operator):
+	'''Load a 3x3 lookAt matrix from file'''
+	bl_idname = "luxrender.lookat_load"
+	bl_label = "Load Camera LookAt"	
+
+	directory		= bpy.props.StringProperty(name='LXS directory')
+	filename   	 	= bpy.props.StringProperty(name='LXS filename')
+
+	def readfile(self,file_name):
+		FILE = open(file_name, "r")
+		transf = []
+		lines = FILE.readlines()
+		for line in lines:
+			data = line.split()
+			for i in data:
+				transf.append(float(i))
+		FILE.close()
+		return transf
+
+	def invoke(self, context, event):
+		context.window_manager.fileselect_add(self)
+		return {'RUNNING_MODAL'}
+
+	def execute(self, context):
+		lkt_filename = '/'.join([
+			self.properties.directory,
+			self.properties.filename
+		])
+		LKT = self.readfile(lkt_filename)
+
+		scene = context.scene
+		obj_act = scene.objects.active
+
+		obj_act.location[0] = LKT[0] 
+		obj_act.location[1] = LKT[1]
+		obj_act.location[2] = LKT[2]
+		obj_act.rotation_mode = 'XYZ'
+
+		Dir = [ LKT[3]-LKT[0], LKT[4]-LKT[1], LKT[5]-LKT[2] ]
+		Right = [ Dir[1]*LKT[8]-Dir[2]*LKT[7], Dir[2]*LKT[6]-Dir[0]*LKT[8], Dir[0]*LKT[7]-Dir[1]*LKT[6] ]
+		
+		M = [ Right[0], Right[1], Right[2], LKT[0], LKT[6], LKT[7], LKT[8], LKT[1], -Dir[0], -Dir[1], -Dir[2], LKT[2], 0, 0, 0, 1 ]
+		
+		et = [ 0, 0, 0 ]
+
+		cy = sqrt( M[0]*M[0] + M[1]*M[1] )
+	
+		if cy > 0:
+			et[0] = atan2( M[6], M[10] )
+			et[1] = atan2( -M[2], cy )
+			et[2] = atan2( M[1], M[0] )
+
+		else :
+			et[0] = atan2( -M[9], M[5] )
+			et[1] = atan2( -M[2], cy )
+			et[2] = 0
+
+		obj_act.rotation_euler[0] = et[0]
+		obj_act.rotation_euler[1] = et[1]
+		obj_act.rotation_euler[2] = et[2]
+		return {'FINISHED'}
+
+
+class LUXRENDER_OT_transform_file(bpy.types.Operator):
+	'''Load a Transfrom matrix from file'''
+	bl_idname = "luxrender.transform_load"
+	bl_label = "Load Transform Matrix"	
+
+	directory		= bpy.props.StringProperty(name='LXS directory')
+	filename   	 	= bpy.props.StringProperty(name='LXS filename')
+
+	def readfile(self,file_name):
+		FILE = open(file_name, "r")
+		transf = []
+		lines = FILE.readlines()
+		for line in lines:
+			data = line.split()
+			for i in data:
+				transf.append(float(i))
+		FILE.close()
+		return transf
+
+	def invoke(self, context, event):
+		context.window_manager.fileselect_add(self)
+		return {'RUNNING_MODAL'}
+
+	def execute(self, context):
+		lkt_filename = '/'.join([
+			self.properties.directory,
+			self.properties.filename
+		])
+
+		scene = context.scene
+		obj_act = scene.objects.active
+
+		M = self.readfile(lkt_filename)
+
+		et = [ 0, 0, 0 ]
+
+		cy = sqrt( M[0]*M[0] + M[1]*M[1] )
+	
+		if cy > 0:
+			et[0] = atan2( M[6], M[10] )
+			et[1] = atan2( -M[2], cy )
+			et[2] = atan2( M[1], M[0] )
+
+		else :
+			et[0] = atan2( -M[9], M[5] )
+			et[1] = atan2( -M[2], cy )
+			et[2] = 0
+
+		obj_act.location[0] = M[3] 
+		obj_act.location[1] = M[7]
+		obj_act.location[2] = M[11]
+		obj_act.rotation_euler[0] = et[0]
+		obj_act.rotation_euler[1] = et[1]
+		obj_act.rotation_euler[2] = et[2]
+		obj_act.rotation_mode = 'XYZ'
+		return {'FINISHED'}
+
+# Export process
+
+class EXPORT_OT_luxrender(bpy.types.Operator):
+	bl_idname = 'export.luxrender'
+	bl_label = 'Export LuxRender Scene (.lxs)'
+	
+	filename		= bpy.props.StringProperty(name='LXS filename')
+	directory		= bpy.props.StringProperty(name='LXS directory')
+	
+	api_type		= bpy.props.StringProperty(options={'HIDDEN'}, default='FILE')	# Export target ['FILE','API',...]
+	write_files		= bpy.props.BoolProperty(options={'HIDDEN'}, default=True)		# Write any files ?
+	write_all_files	= bpy.props.BoolProperty(options={'HIDDEN'}, default=True)		# Force writing all files, don't obey UI settings
+	
+	scene			= bpy.props.StringProperty(options={'HIDDEN'}, default='')		# Specify scene to export
+	
+	def invoke(self, context, event):
+		context.window_manager.fileselect_add(self)
+		return {'RUNNING_MODAL'}
+	
+	def execute(self, context):
+		if self.properties.scene == '':
+			scene = context.scene
+		else:
+			scene = bpy.data.scenes[self.properties.scene]
+		
+		return SceneExporter() \
+				.set_report(self.report) \
+				.set_properties(self.properties) \
+				.set_scene(scene) \
+				.export()
+
+menu_func = lambda self, context: self.layout.operator("export.luxrender", text="Export LuxRender Scene...")
+bpy.types.INFO_MT_file_export.append(menu_func)
---
+++ src/luxrender/properties/mesh.py
@@ -0,0 +1,217 @@
+# -*- coding: utf8 -*-
+#
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+# --------------------------------------------------------------------------
+# Blender 2.5 LuxRender Add-On
+# --------------------------------------------------------------------------
+#
+# Authors:
+# Doug Hammond, Daniel Genrich
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# ***** END GPL LICENCE BLOCK *****
+#
+from extensions_framework import declarative_property_group
+from extensions_framework.validate import Logic_OR as O, Logic_Operator as LO
+
+from luxrender.export import ParamSet
+from luxrender.properties.material import dict_merge, texture_append_visibility
+from luxrender.properties.texture import FloatTextureParameter
+
+class MeshFloatTextureParameter(FloatTextureParameter):
+	def texture_slot_set_attr(self):
+		# Looks in a different location than other FloatTextureParameters
+		return lambda s,c: c.luxrender_mesh
+
+TF_displacementmap = MeshFloatTextureParameter(
+	'dm',
+	'Displacement Map',
+	real_attr='displacementmap',
+	add_float_value=False
+)
+
+def mesh_visibility():
+	
+	vis = dict_merge({
+		'ccam':		{'projection': True },
+		'ucam': 	{'projection': True , 'ccam': False },
+		'nsmooth':		{ 'subdiv': LO({'!=': 'None'}) },
+		'sharpbound':	{ 'subdiv': LO({'!=': 'None'}) },
+		'sublevels':	{ 'subdiv': LO({'!=': 'None'}) },
+		'dmscale':		{ 'subdiv': LO({'!=': 'None'}), 'dm_floattexturename': LO({'!=': ''}) },
+		'dmoffset':		{ 'subdiv': LO({'!=': 'None'}), 'dm_floattexturename': LO({'!=': ''}) },
+	}, TF_displacementmap.visibility )
+	
+	vis = texture_append_visibility(vis, TF_displacementmap, { 'subdiv': LO({'!=': 'None'}) })
+	
+	return vis
+
+class luxrender_mesh(declarative_property_group):
+	'''
+	Storage class for LuxRender Camera settings.
+	This class will be instantiated within a Blender
+	mesh object.
+	'''
+	
+	controls = [
+		'mesh_type',
+		['portal', 'AR_enabled'],
+		['projection', 'ccam'],
+		'ucam',
+		['subdiv','sublevels'],
+		['nsmooth', 'sharpbound'],
+	] + \
+		TF_displacementmap.controls + \
+	[
+		['dmscale', 'dmoffset']
+	]
+	
+	visibility = mesh_visibility()
+	
+	properties = [
+		{
+			'type': 'enum',
+			'attr': 'mesh_type',
+			'name': 'Export as',
+			'items': [
+				('global', 'Use default setting', 'global'),
+				('native', 'LuxRender mesh', 'native'),
+				('binary_ply', 'Binary PLY', 'binary_ply')
+			],
+			'default': 'global'
+		},
+		{
+			'type': 'bool',
+			'attr': 'portal',
+			'name': 'Exit Portal',
+			'default': False,
+		},
+		{
+			'type': 'enum',
+			'attr': 'subdiv',
+			'name': 'Subdivision Scheme',
+			'default': 'None',
+			'items': [
+				('None', 'None', 'None'),
+				('loop', 'loop', 'loop'),
+				('microdisplacement', 'microdisplacement', 'microdisplacement')
+			]
+		},
+		{
+			'type': 'bool',
+			'attr': 'nsmooth',
+			'name': 'Normal smoothing',
+			'default': True,
+		},
+		{
+			'type': 'bool',
+			'attr': 'sharpbound',
+			'name': 'Sharpen Bounds',
+			'default': False,
+		},
+		{
+			'type': 'int',
+			'attr': 'sublevels',
+			'name': 'Subdivision Levels',
+			'default': 2,
+			'min': 0,
+			'soft_min': 0,
+			'max': 300,
+			'soft_max': 300
+		},
+		{ 
+			'type': 'bool',
+			'attr': 'AR_enabled',
+			'name': 'Is an AR surface',
+			'description': 'Using for declare the object as support object in Augmented Reality Scene',
+			'default': False,
+		},
+		{
+			'type': 'bool',
+			'attr': 'projection',
+			'name': 'Use projector texture',
+			'description': 'Select the projector texture type for the object',
+			'default': False,
+		},
+		{
+			'type': 'bool',
+			'attr': 'ccam',
+			'name': 'Projection point at current Camera',
+			'description': 'Select the position of current camera as the projection point for texture',
+			'default': False,
+		},
+		{
+			'type': 'float_vector',
+			'attr': 'ucam',
+			'name': 'Define Projection point',
+			'description': 'Define the projection point for texture',
+			'default': (0.0, 0.0, 0.0),
+			'save_in_preset': True,
+		},
+	] + \
+		TF_displacementmap.properties + \
+	[
+		{
+			'type': 'float',
+			'attr': 'dmscale',
+			'name': 'Scale',
+			'description': 'Displacement Map Scale',
+			'default': 1.0,
+			'precision': 6,
+		},
+		{
+			'type': 'float',
+			'attr': 'dmoffset',
+			'name': 'Offset',
+			'description': 'Displacement Map Offset',
+			'default': 0.0,
+			'precision': 6,
+		},
+	]
+	
+	def get_shape_IsSupport(self):
+		return self.AR_enabled
+	
+	def get_paramset(self, scene):
+
+		params = ParamSet()
+		
+		# check if subdivision is used
+		if self.subdiv != 'None':
+			params.add_string('subdivscheme', self.subdiv)
+			params.add_integer('nsubdivlevels',self.sublevels)
+			params.add_bool('dmnormalsmooth', self.nsmooth)
+			params.add_bool('dmsharpboundary', self.sharpbound)
+			
+		
+		export_dm = TF_displacementmap.get_paramset(self)
+		
+		if self.dm_floattexturename != '' and len(export_dm) > 0:
+			params.add_string('displacementmap', self.dm_floattexturename)
+			params.add_float('dmscale', self.dmscale)
+			params.add_float('dmoffset', self.dmoffset)
+
+		if self.AR_enabled:
+			params.add_bool('support', self.AR_enabled)
+		if self.projection:
+			params.add_bool('projection', self.projection)
+			if self.ccam:
+				cam_pos =  scene.camera.data.luxrender_camera.lookAt(scene.camera)
+				params.add_point('cam', ( cam_pos[0], cam_pos[1], cam_pos[2] ) )
+			else:
+				params.add_point('cam', self.ucam)
+		
+		return params
